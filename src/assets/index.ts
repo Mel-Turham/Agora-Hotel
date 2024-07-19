9921b04f999c1ea55d32ad938b6a7b6964df027d
Mel/ <tchoikuemel06@gmail.com>
2024-07-19 17:38:19 +0100

    type files for each room

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+import Deluxe from './images/Deluxe.jpg';
+import Single from './images/single.jpg';
+import KingRome from './images/king-room.jpg';
+import FamilyRome from './images/family-room.jpg';
+import Honeymoon from './images/honeymoon.jpg';
+
+export { Deluxe, Single, KingRome, FamilyRome, Honeymoon };
