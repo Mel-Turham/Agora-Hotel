f12ae2241050c0501f118faca61d48e604f1d767
Mel/ <tchoikuemel06@gmail.com>
2024-08-09 22:00:56 +0100

    import images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,29 @@ import Single from './images/single.jpg';
 import KingRome from './images/king-room.jpg';
 import FamilyRome from './images/family-room.jpg';
 import Honeymoon from './images/honeymoon.jpg';
+import Restarant from './images/restaurant.jpg';
+import Spa from './images/spa.jpg';
+import Swimming from './images/swimming.jpg';
+import Conference from './images/conference.jpg';
+import RestarantIcon from './images/conference.png';
+import SpaIcon from './images/spa.png';
+import SwimmingIcon from './images/swimmer.png';
+import ConferenceIcon from './images/conference.png';
+import Plate from './images/plate.png'
 
-export { Deluxe, Single, KingRome, FamilyRome, Honeymoon };
+export {
+	SpaIcon,
+	RestarantIcon,
+	Deluxe,
+	Single,
+	KingRome,
+	FamilyRome,
+	Honeymoon,
+	Restarant,
+	Spa,
+	Swimming,
+	Conference,
+	ConferenceIcon,
+	SwimmingIcon,
+  Plate
+};
